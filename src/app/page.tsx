e9c711e331e500aab40ec5ce6186186c79389ece
Piyush Choudhari <choudhari.piyush@gmail.com>
2025-01-04 03:12:23 +0000

    fixed image bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,6 +75,7 @@ export default function Home() {
           <main className="p-8 xl:p-12 xl:pr-[10%] flex-grow">
             <About />
             <Skills />
+            <Experience />
             <Education />
             <Projects />
           </main>
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from "next/legacy/image";
-import { ChipList} from './Chips';
+import { ChipList } from './Chips';
 
 interface Job {
   title: string;
@@ -14,33 +14,15 @@ interface Job {
 
 const experiences: Job[] = [
   {
-    title: "AI Engineer - Intern",
+    title: "AI Engineer Intern",
     company: "Ronin Labs Pvt Limited.",
     location: "Pune, India",
     period: "Jan 2026 - Present",
     description: [
-      "Led the development of an innovative AI-powered shift scheduling platform for the hospitality industry.",
-      "Architected and implemented a scalable microservices-based backend using Node.js and GraphQL.",
-      "Designed and developed a responsive front-end application using React and Next.js.",
-      "Integrated advanced machine learning algorithms for intelligent shift allocation and demand forecasting.",
-      "Managed a team of developers, fostering a culture of innovation and continuous improvement."
+      "TBD",
     ],
     logo: "/ronin_labs.jpg",
-    skills: ["NextJS", "Supabase", "React Native", "Expo", "Machine Learning", "Tauri"]
-  },
-  {
-    title: "Software Development Intern",
-    company: "Preezie",
-    location: "Melbourne, Victoria",
-    period: "November 2022 - March 2023",
-    description: [
-      "Demonstrated proficiency in Angular 4, React, .NET, and microservices.",
-      "Developed effective solutions to complex issues and debugged code efficiently.",
-      "Adapted to fast-paced development environments and worked with both legacy and modern technologies.",
-      "Collaborated with cross-functional teams, enhancing communication and teamwork skills."
-    ],
-    logo: "/preezie.png",
-    skills: ["Angular 4", "React", ".NET", "Microservices"]
+    skills: ["Python", "Transformer Technologies"]
   },
 ];
 
@@ -53,11 +35,11 @@ const Experience: React.FC = () => {
           <div key={index} className="relative pl-8 before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-0.5 before:bg-gray-600">
             <div className="flex items-start mb-2">
               <div className="bg-white rounded-full p-2 mr-4 w-24 h-24 flex-shrink-0 flex items-center justify-center overflow-hidden">
-                <Image 
-                  src={job.logo} 
-                  alt={`${job.company} logo`} 
-                  width={40} 
-                  height={40} 
+                <Image
+                  src={job.logo}
+                  alt={`${job.company} logo`}
+                  width={40}
+                  height={40}
                   className="object-contain"
                 />
               </div>
