982c216db60503a4ef6b8652e82cab34aaae8dcd
Piyush Choudhari <choudhari.piyush@gmail.com>
2025-07-19 10:49:02 +0530

    Update ResumeButton.tsx

--- src/components/ResumeButton.tsx
+++ src/components/ResumeButton.tsx
@@ -18,7 +18,7 @@ const ResumeButton: React.FC = () => {
         <TooltipTrigger asChild>
           <Button variant="outline" size="sm" className="gap-2" asChild>
             <a
-              href="https://drive.google.com/file/d/124C6O6aeoRX4TOs0gUv-N1jsel6bDmy_/view?usp=sharing"
+              href="https://drive.google.com/file/d/1DF7Voxov4Q6JHtCUdY-V69vqh8ALCAbs/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
